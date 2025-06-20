ea03d15187c09da89be74f68d457ce93338f543c
000ashishYadav <000ashishyadav2003@gmail.com>
2025-06-20 14:57:50 +0530

    title changed

--- src/app/components/homePage/Header.jsx
+++ src/app/components/homePage/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
           <div className="flex items-center space-x-2">
             <Compass className="h-8 w-8 text-blue-600" />
             <span className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">
-              VoiceFind
+              Guide Mate
             </span>
           </div>
           <div className="hidden md:flex items-center space-x-6">
